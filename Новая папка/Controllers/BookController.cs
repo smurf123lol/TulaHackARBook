357d506c1cf3d1190b28d68b560c9645103b31f0
Александр <Volosnikov.aleksandr2@gmail.com>
2023-10-21 18:53:39 +0500

    AddBack

---
+++ Новая папка/Controllers/BookController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using VirtualBook.Entity;
+
+namespace VirtualBook.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BookController : ControllerBase
+    {
+        [Route("GetBookByName")]
+        [HttpGet]
+        public async Task<ActionResult<List<Book>>> GetBookByName(string partName)
+        {
+
+            DBConnect db = new();
+            List<Book> books = db.Books.Where(book => EF.Functions.Like(book.Name, $"%{partName}%")).ToList();
+
+            if (books.Count == 0)
+                this.HttpContext.Response.StatusCode = 404;
+
+            return books;
+        }
+        [Route("GetBookByAuthor")]
+        [HttpGet]
+        public async Task<ActionResult<List<Book>>> GetBookByAuthor(string author)
+        {
+            DBConnect db = new();
+            List<Book> books = db.Books.Where(book => EF.Functions.Like(book.Author, $"%{author}%")).ToList();
+
+            if (books.Count == 0)
+                this.HttpContext.Response.StatusCode = 404;
+
+            return books;
+        }
+        [Route("GetTitlesByBookName")]
+        [HttpGet]
+        public async Task<ActionResult<List<BookTitles>>> GetTitlesByBookName(string BookName)
+        {
+            DBConnect db = new();
+            List<BookTitles> tytles = db.BookTytles.Where(tytle => tytle.Book.Name == BookName).ToList();
+
+            if (tytles.Count == 0)
+                this.HttpContext.Response.StatusCode = 404;
+
+            return tytles;
+        }
+        [Route("GetTitlesByBookId")]
+        [HttpGet]
+        public async Task<ActionResult<List<BookTitles>>> GetTitlesByBookId(int BookId)
+        {
+            DBConnect db = new();
+            List<BookTitles> tytles = db.BookTytles.Where(tytle => tytle.Book.Id == BookId).ToList();
+
+            if (tytles.Count == 0)
+                this.HttpContext.Response.StatusCode = 404;
+
+            return tytles;
+        }
+    }
+}
---
+++ Новая папка/Entity/Book.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace VirtualBook.Entity
+{
+    [PrimaryKey("Id")]
+    [Table("Books")]
+    public class Book
+    {
+        public Book(string name, string author = "unknow", string description = "unknow", string imageURL = "http://localhost:5216/Book/Storage/placeHolder.png")
+        {
+            Name = name;
+            Author = author;
+            Description = description;
+            ImageURL = imageURL;
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Author { get; set; }
+        public string Description { get; set; }
+        public string ImageURL { get; set; }
+    }
+}

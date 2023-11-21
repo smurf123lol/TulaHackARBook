8a7cdd5b3ccd1823c9bc45d348f6c9aa42aff2b2
Александр <Volosnikov.aleksandr2@gmail.com>
2023-11-21 23:00:40 +0500

    Update BookController.cs

--- Новая папка/Controllers/BookController.cs
+++ Новая папка/Controllers/BookController.cs
@@ -13,7 +13,7 @@ namespace VirtualBook.Controllers
         public async Task<ActionResult<List<Book>>> GetBookByName(string partName)
         {
 
-            DBConnect db = new();
+            using DBConnect db = new();
             List<Book> books = db.Books.Where(book => EF.Functions.Like(book.Name, $"%{partName}%")).ToList();
 
             if (books.Count == 0)
@@ -25,11 +25,11 @@ namespace VirtualBook.Controllers
         [HttpGet]
         public async Task<ActionResult<List<Book>>> GetBookByAuthor(string author)
         {
-            DBConnect db = new();
+            using DBConnect db = new();
             List<Book> books = db.Books.Where(book => 
-                EF.Functions.Like(book.Author.FirtstName, $"%{author}%") ||
+                EF.Functions.Like(book.Author.FirstName, $"%{author}%") ||
                 EF.Functions.Like(book.Author.LastName, $"%{author}%")   ||
-                EF.Functions.Like(book.Author.MiddleName, $"%{author}%")
+                EF.Functions.Like(book.Author.MiddleName, $"%{author}%")   
             ).ToList();
 
             if (books.Count == 0)
@@ -41,7 +41,7 @@ namespace VirtualBook.Controllers
         [HttpGet]
         public async Task<ActionResult<List<BookTytle>>> GetTitlesByBookId(int BookId)
         {
-            DBConnect db = new();
+            using DBConnect db = new();
             List<BookTytle> tytles = db.BookTytles.Where(tytle => tytle.Book.Id == BookId).ToList();
 
             if (tytles.Count == 0)
@@ -53,7 +53,7 @@ namespace VirtualBook.Controllers
         [HttpGet]
         public async Task<ActionResult<List<ARObject>>> GetEntity(int BookId, int TytleId, string partName)
         {
-            DBConnect db = new();
+            using DBConnect db = new();
             List<ARObject> objects = db.ARObjects.Where(obj => obj.Book.Id == BookId && obj.BookTytle.Id == TytleId && EF.Functions.Like(obj.Name, $"%{partName}%")).ToList();
 
             if (objects.Count == 0)
